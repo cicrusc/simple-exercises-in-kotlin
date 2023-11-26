64ae87b465b5f60de704e832730745360839347f
dvdr <ro.david@hotmail.it>
2023-11-27 00:57:37 +0100

    Upload Exercise2

---
+++ src/main/kotlin/exercise2.kt
@@ -0,0 +1,14 @@
+/*2. Basic literals: numbers, strings, and characters
+Esercizio: Crea un programma che dichiari una variabile number di tipo Int inizializzata a 10,
+una variabile text di tipo String inizializzata a "Kotlin", e una variabile character di tipo Char inizializzata a 'K'.
+Stampa ciascuna di queste variabili.*/
+
+fun main() {
+    val number = 1
+    val string = "Kotlin"'
+    val char = 'K'
+
+    println(number)
+    println(string)
+    println(char)
+}
