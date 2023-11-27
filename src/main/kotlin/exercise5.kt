0ddf18fc797a7e8d37079d62f7c281a31079b7fc
dvdr <ro.david@hotmail.it>
2023-11-27 23:31:31 +0100

    Upload Exercise5

---
+++ src/main/kotlin/exercise5.kt
@@ -0,0 +1,11 @@
+/*5. Constants and val Variables
+Define a constant val PI with the value 3.14. Use PI to calculate and print the circumference of a circle given its radius.*/
+
+const val PI = 3.14159265359
+
+fun main() {
+    val radius = 5
+    val circumference = 2 * PI * radius
+
+    println("The circumference of the cirle is $circumference")
+}
