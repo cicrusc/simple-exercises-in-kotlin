33af78bd984d4c8602c104bfe92f4a2f31ef4671
dvdr <ro.david@hotmail.it>
2023-11-28 00:10:08 +0100

    Upload Exercise6

---
+++ src/main/kotlin/exercise6.kt
@@ -0,0 +1,15 @@
+/*6. Standard Output
+Write a program that prints various data types (such as Int, String, Double) using println and illustrates
+the difference in handling these types during printing.*/
+
+fun main() {
+    val number: Int = 10
+    println("The number is $number")
+
+    val text: String = "Hello Kotlin!"
+    println("The text is $text")
+
+    val decimal: Double = 3.14
+    println("The decimal is $decimal")
+
+}
