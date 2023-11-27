7c3eab14b17999b66928c9d8fc75a653e50de90c
cicrusc <45275309+cicrusc@users.noreply.github.com>
2023-11-27 01:00:22 +0100

    Update exercise2.kt

--- src/main/kotlin/exercise2.kt
+++ src/main/kotlin/exercise2.kt
@@ -1,7 +1,8 @@
 /*2. Basic literals: numbers, strings, and characters
-Esercizio: Crea un programma che dichiari una variabile number di tipo Int inizializzata a 10,
-una variabile text di tipo String inizializzata a "Kotlin", e una variabile character di tipo Char inizializzata a 'K'.
-Stampa ciascuna di queste variabili.*/
+Exercise: Create a program that declares a variable number of type Int initialized to 10,
+a variable text of type String initialized to "Kotlin", and a variable character of type Char initialized to 'K'.
+Print each of these variables.*/
+
 
 fun main() {
     val number = 1
