b82c0432443a55838c0c7b370667741ae8cbe187
dvdr <ro.david@hotmail.it>
2023-11-27 23:18:25 +0100

    Upload Execise4

---
+++ src/main/kotlin/exercise4.kt
@@ -0,0 +1,10 @@
+/*4. Values and Variables
+Create two variables: an immutable val with your age and a mutable var with your favorite number. Print both.*/
+
+fun main() {
+    val myAge = 22
+    var prefNumber = 7
+
+    println(myAge)
+    println(prefNumber)
+}
