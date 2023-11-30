01025fe1258ea6e66d16fde8613b26233506173a
dvdr <ro.david@hotmail.it>
2023-11-30 23:43:39 +0100

    Rimuovo prova.kt

--- src/main/kotlin/prova.kt
+++
@@ -1,8 +0,0 @@
-/*The program is to output the numbers from this interval, but if the number is divisible by 3,
-you should output Fizz instead of it; if the number is divisible by 5, output Buzz,
-and if it is divisible both by 3 and by 5, output FizzBuzz.Output each number or word on a separate line.*/
-
-fun main() {
-    val just_a_value = 0
-
-}
