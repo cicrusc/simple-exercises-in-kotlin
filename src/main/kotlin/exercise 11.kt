9cd3cb44427fc74ea37eb272e37a7c8126cd23f5
dvdr <ro.david@hotmail.it>
2023-12-07 01:04:39 +0100

    Upload exercise 11

---
+++ src/main/kotlin/exercise 11.kt
@@ -0,0 +1,17 @@
+/*1. Comments
+Write a program that uses comments to explain the functioning of the code.
+Use both single-line (//) and multi-line (/* */) comments to describe each section of the program.
+ */
+
+fun main() {
+    val number1 = 1 //first number
+    val number2 = 20 //second number
+
+    //Calculate the sum
+    val sum = number1 + number2
+
+    /*Stamp the result
+    This is an example of multiline comments
+     */
+    println("The sum of $number1 + $number2 is $sum.")
+}
